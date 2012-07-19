3492581b5bf763e9a5d12754a51854d20dd9ac3f
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-07-19 13:57:28 +0400

    bugfixes + abstraction of behaviour

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ACTION=$1
-ICON_PATH="$( pwd )"
+TIME=$2
+ICON_PATH="$( dirname $( readlink $0 ) )"
 EXT="png"
 NOTIFY_OPTIONS=''
 
@@ -16,6 +17,10 @@ EOF
   exit 1
 }
 
+check_string() {
+  echo "$1" | egrep -qv '(;|&|\||<|>)' || \
+    fail "$1 contains special characters:  ;&|<>"
+}
 
 fail() {
   echo "Error: $*"
@@ -35,31 +40,41 @@ message() {
 }
 
 set_alarm() {
-  [ -z "$2" ] && fail "Usage: set_alarm() ACTION TIME"
+  [ -z "$2" ] && fail "Usage: set_alarm() ACTION TIME REPEAT"
 
-  at "now+$2min" << EOF
-    DISPLAY=$DISPLAY notify-send -i "$( icon_name $1 )" $NOTIFY_OPTIONS "$( message $1)"
-EOF
+  (
+    echo "DISPLAY=$DISPLAY notify-send -i \"$( icon_name $1 )\" $NOTIFY_OPTIONS \"$( message $1)\""
+    [ -n "$REPEAT" ] && echo "DISPLAY=$DISPLAY POMODORO_CONFIG=\"$POMODORO_CONFIG\" $0 $1"
+  ) |  at "now+$2min"
 }
 
 ### Main program starts here
 
 [ -z "$ACTION" ] && usage
 
+check_string "$ACTION"
+
 which notify-send > /dev/null || fail "notify-send not found. 'apt-get install libnotify-bin' is likely to help you"
 
-case $ACTION in
-  work)
-    TIME=${2:-40}
-    NEXT_ACTION='rest'
-    ;;
-  rest)
-    TIME=${2:-10}
-    NEXT_ACTION='work'
-    ;;
-  *)
-    fail "Unknown action: $ACTION"
-    ;;
-esac
+[ -n "$POMODORO_CONFIG" ] && . "$POMODORO_CONFIG"
 
-set_alarm $NEXT_ACTION $TIME
+if [ -z "$ACTIONS" ]; then
+  ACTIONS="work rest"
+  [ -z "$work_duration" ] && work_duration=40
+  [ -z "$rest_duration" ] && rest_duration=10
+  work_nextaction=rest
+  rest_nextaction=work
+fi
+
+for action in $(echo $ACTIONS); do
+  if [ "x$ACTION" = "x$action" ]; then
+    ACTION_FOUND=1
+    TIME=${TIME:-"$( eval echo "\$${action}_duration" )"}
+    NEXT_ACTION="$( eval echo "\$${action}_nextaction" )"
+    break
+  fi
+done
+
+[ -z "$ACTION_FOUND" ] && fail "Unknown action: ${ACTION}"
+
+set_alarm $NEXT_ACTION $TIME $REPEAT
